31d2705678722d6581e4b3f28fb92745e8dad5ef
JoSebas007 <joangutierrez2001@gmail.com>
2022-05-14 18:53:22 -0500

    Css file and Gif image added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,6 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-p#l%sxuh@p@&^(lx@p5w735#6v$%*ham@&nx$l^1a$uw)qcq)@'
 
 SECRET_KEY = env('SECRET_KEY', default='unsafe secret key')
 
